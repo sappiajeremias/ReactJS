0c2247d54f2dbc721e1a685754a705fc19d1bc4b
sappiajeremias <jeremiassappia@gmail.com>
2022-11-10 10:34:56 -0300

    Actividad 1 finalizada

--- Act1/formulario.js
+++ Act1/formulario.js
@@ -1,10 +1,11 @@
 function mostrarResultados() {
     var nombre = document.getElementById("nombre").value;
     var mail = document.getElementById("mail").value;
-    if (nombre == "" || mail == "") {
+    var tel = document.getElementById("telefono").value;
+    if (nombre == "" || mail == "" || tel == "") {
         alert('No completaste todos los datos!');
     } else {
-        alert(`Gracias por enviar tus datos! Nombre: ${nombre}. Mail: ${mail}`);
+        alert(`Gracias por enviar tus datos! Nombre: ${nombre}. Mail: ${mail}. Telefono ${tel}`);
         resetForm();
     }
 }
